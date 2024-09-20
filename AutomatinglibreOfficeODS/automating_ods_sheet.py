6ea1de6f49cc44396223c54724154d1a463fa744
Godfred Abora <aboragodfred2@gmail.com>
2024-09-20 15:55:55 +0000

    working aroung

--- AutomatinglibreOfficeODS/automating_ods_sheet.py
+++ AutomatinglibreOfficeODS/automating_ods_sheet.py
@@ -4,26 +4,24 @@ def StudentScores(filename):
     data = ods.get_data(filename)
     sheet = data['Sheet1']  # Sheet data is a list of rows
 
-   
     if len(sheet) < 2:
-       print("Not enough data to process.")
+        print("Not enough data to process.")
+        return
 
-    for row in range(1, len(sheet)):
-        # Ensure each row has enough columns (at least 4 for input, will add more)
-        if len(sheet[row]) >= 4:
+    for row in range(2, len(sheet)):
             mid_sem_score = sheet[row][2]  # 3rd column (index 2)
             end_sem_score = sheet[row][3]  # 4th column (index 3)
 
             # Perform calculations
             mid_sem_30 = mid_sem_score * 0.30
             end_sem_70 = end_sem_score * 0.70
+            total_score = mid_sem_30 + end_sem_70
 
-            # Append the calculated values to the row
-            sheet[row][4].append(mid_sem_30)
-            sheet[row][5].append(end_sem_70)
-            sheet[row][6].append(mid_sem_30 + end_sem_70)
-        # else:
-        #     print(f"Row {row + 1} does not have enough columns to process.")
+            # Store the calculated values in the correct columns
+            sheet[row][3] = mid_sem_30   # 4th column (index 3)
+            sheet[row][4] = end_sem_70   # 5th column (index 4)
+            sheet[row][5] = total_score  # 6th column (index 5)
+
 
     # Save the changes back to the file
     ods.save_data(filename, data)
